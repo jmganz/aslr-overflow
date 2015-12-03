c7ea097777f12efc40d5658cbd45a19ec9c1102e
Jonathan Ganz <jmganz@ucdavis.edu>
2015-12-03 13:11:30 -0800

    Document runClient.sh and runServer.sh

--- runClient.sh
+++ runClient.sh
@@ -2,13 +2,16 @@
 
 ulimit -c 0
 
+# LOOP 800 TIMES
 for run in `jot 800 1`
 do
   printf \\r$run
+  # THE FOLLOWING COMMENTED CODE WORKS BETTER IN LINUX
   #./client >> $1 &
   #clientPID=$!
   #( sleep 200; kill $clientPID 2> /dev/null ) &
   #wait $clientPID
+  # KILL CLIENT AFTER 60 SECONDS
   timeout -s 9 60 ./client >> $1
   sleep 2
 done
--- runServer.sh
+++ runServer.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+# CATCHING AND IGNORING SIGNAL 15 HELPS AVOID HANGS
 trap "" 15
 
 ulimit -c 0
 
+# LOOP FOREVER
 while true
 do
+  # THE FOLLOWING COMMENTED CODE WORKS BETTER IN LINUX
   #./server &
   #serverPID=$!
   #( sleep 200; kill $serverPID 2> /dev/null ) &
   #wait $serverPID
+  # KILL CLIENT AFTER 60 SECONDS
   timeout -s 9 60 ./client >> $1
   sleep 1
 done
