b51a93622efff8f6da46a2765b0bcaa9a77649bb
Jonathan Ganz <jmganz@ucdavis.edu>
2015-12-03 13:35:43 -0800

    Change a comment in runServer.sh to reflect the previous push

--- runServer.sh
+++ runServer.sh
@@ -13,7 +13,7 @@ do
   #serverPID=$!
   #( sleep 200; kill $serverPID 2> /dev/null ) &
   #wait $serverPID
-  # KILL CLIENT AFTER 60 SECONDS
+  # KILL SERVER AFTER 60 SECONDS
   timeout -s 9 60 ./server
   sleep 1
 done
